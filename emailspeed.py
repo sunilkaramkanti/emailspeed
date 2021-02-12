ad3059f2f0ac77f0c43b7233cd72eccca28bff6a
sunil karamkanti <sunilkaramkanti@gmail.com>
2021-02-12 15:27:57 +0530

    intial commit

--- emailspeed.py
+++ emailspeed.py
@@ -1,12 +1,10 @@
 import speedtest
-import smtplib
+
 def sendspeed():
     st =speedtest.Speedtest()
-    print(st)
     print(st.download())
     print(st.upload())
     servernames =[]
     st.get_servers(servernames)
     print(st.results.ping)
-
 sendspeed()
